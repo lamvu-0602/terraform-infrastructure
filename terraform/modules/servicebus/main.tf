d6b5c73af91e281267935c20a9c220d06dd0d7e8
Vu Tung Lam <lamvu@kms-technology.com>
2026-06-06 22:50:52 +0700

    remove duplicate detection because of using Basic tier

--- terraform/modules/servicebus/main.tf
+++ terraform/modules/servicebus/main.tf
@@ -12,6 +12,6 @@ resource "azurerm_servicebus_queue" "learning" {
 
   max_delivery_count                      = 3
   requires_session                        = true
-  requires_duplicate_detection            = true
-  duplicate_detection_history_time_window = "PT10M"
+  # requires_duplicate_detection            = true | remove because using 'Basic' tier
+  # duplicate_detection_history_time_window = "PT10M"
 }
