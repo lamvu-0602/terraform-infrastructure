a3aa6bcea2afea9c2f0516006ad19edc5d337a3c
Vu Tung Lam <lamvu@kms-technology.com>
2026-06-06 22:51:46 +0700

    fmt

--- terraform/modules/servicebus/main.tf
+++ terraform/modules/servicebus/main.tf
@@ -10,8 +10,8 @@ resource "azurerm_servicebus_queue" "learning" {
   namespace_id          = azurerm_servicebus_namespace.asbn.id
   max_size_in_megabytes = 1024
 
-  max_delivery_count                      = 3
-  requires_session                        = true
+  max_delivery_count = 3
+  requires_session   = true
   # requires_duplicate_detection            = true | remove because using 'Basic' tier
   # duplicate_detection_history_time_window = "PT10M"
 }
